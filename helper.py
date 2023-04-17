2816f2ccb035f7a6733ceafd51f2decff3570292
Abdur-Rahman-Khan <ark_probook@ARKhan>
2023-04-18 04:43:17 +0530

    Refactored Working

--- helper.py
+++
@@ -1,3 +0,0 @@
-import test.py as test
-def makeMove(player=1):
-    test.print_board()
--- mover.py
+++ mover.py
@@ -1,24 +1,27 @@
-import test
+import core
 import copy
 
-def get_move(board,player):
+def get_move(board, player):
+    return get_move_greedy(board, player)
+   
+def get_move_greedy(board,player):
     # test.print_board(board)
     board_copy = copy.deepcopy(board)
-    valid_moves = test.get_valid_moves(board_copy,player)
+    valid_moves = core.get_valid_moves(board_copy,player)
     if len(valid_moves) == 0:
         return "NO MOVES"
     else:
         #iterate on all moves
         bestMove = valid_moves[0]
-        minScore=  test.get_score(board)[player]
+        minScore=  core.get_score(board)[player]
         for move in valid_moves:
             board_copy = copy.deepcopy(board)
-            test.make_move(board_copy,move[0],move[1],player)
+            core.make_move(board_copy,move[0],move[1],player)
             #check if move is better
-            print("move is: ",move," score is: ",test.get_score(board_copy)[player])
-            if test.get_score(board_copy)[player] > minScore:
+            print("move is: ",move," score is: ",core.get_score(board_copy)[player])
+            if core.get_score(board_copy)[player] > minScore:
                 bestMove = move
-                minScore = test.get_score(board_copy)[player]
+                minScore = core.get_score(board_copy)[player]
                 print("best move is: ",bestMove)
         bestMove = convertToTuple(bestMove)
         return bestMove
